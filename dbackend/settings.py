27d4b592c000d08d7160f24402d07f19e0fc9ba6
Thiruvenkatraja <venkat.tv.raja@gmail.com>
2023-01-27 19:42:57 +0530

    Setting update

--- dbackend/settings.py
+++ dbackend/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import fcntl
+import django
+from django.utils.encoding import smart_str
+django.utils.encoding.smart_text = smart_str
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-shq#mlp7nhm3^+3vf_lj0*(swf)4k#!6hj_-uombdmdxh=18r='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
